fe49860c3721e777f91c82702d4d6e9de3da3d50
student <rubenabustamante@gmail.com>
2020-09-17 19:05:50 -0600

    finsih count words and copy functions

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -4,7 +4,7 @@
 
 int space_char(char c)
 {
-  if(c == '\t' || c == ' '|| c != '\0'){
+  if(((0 != '\t') ||(c == ' '||)) && (c != '\0')){
     return 1;
   }
   return 0;
@@ -12,33 +12,33 @@ int space_char(char c)
 }
 int non_space_char(char c)
 {
-  if(c != '\t' || c != ' '|| c != '\0'){
+  if((c != '\t' ) && ( c != ' ') &&( c != '\0')){
     return 1;
   }
   return 0;
 }
 char *word_start(char *str)
 {
-  char *tmp = str;
-  while(space_char(*tmp)!=1){
-    if(non_space_char(tmp[0])==1){
-      return tmp;
-    }
-    tmp++;
+  int i =0;
+  while(space_char(*(str+1)){
+      if(str[i] !='\0'){
+	return str[i];
+      }
+      i++;
   }
-  return str;
+  return 0;
 }
 char *word_terminator(char *word)
 {
-  
-  char *tmp = word;
-  while(non_space_char(*tmp) ==1){
-    if(non_space_char(tmp[0]) ==0){
-      return tmp;
-    }
-    tmp++;
+  int i =0;
+  char *tmp;
+  while(non_space_char(*(word+1)){
+      if(word[i] == '\0'){
+	tmp  = word[i-1];
+      }
+      i++;
   }
-  return word;
+  return tmp;
 }
 int count_words(char *str)
 {
@@ -49,3 +49,19 @@ int count_words(char *str)
   }
   return n;
 }
+  char *copy_str(char *inStr, short len)
+  {
+    char *ptcopy = malloc((len+1) * sizeof(char));
+    int i;
+
+    for (i = 0 ; i < len; i++){
+      *(ptcopy)  = *(inStr + 1);
+      
+    }
+    return ptcopy;
+  }
+  char **tokenize(char* str)
+  {
+    
+    
+  }
